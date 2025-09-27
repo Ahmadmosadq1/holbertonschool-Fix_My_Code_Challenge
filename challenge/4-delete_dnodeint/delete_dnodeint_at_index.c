dae29e236953a4b6ccea2124eabf0053ee72ba1f
Ahmadmosadq1 <eng.ahmedmosadq@gmail.com>
2025-09-27 06:42:09 +0300

    h

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -20,8 +20,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     if (index == 0)
     {
         *head = temp->next;
-        if (temp->next != NULL)
-            temp->next->prev = NULL;
+        if (*head != NULL)
+            (*head)->prev = NULL;
         free(temp);
         return (1);
     }
